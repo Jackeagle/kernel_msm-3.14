72c4e2e85b6146ea48caa8267a86aea2601c53a2
Alex Elder <elder@codeaurora.org>
2017-11-21 10:29:30 -0600

    msm: gsi: use ENODEV in gsi_dealloc_channel()
    Use ENODEV rather than GSI_STATUS_NODEV in gsi_dealloc_channel().
    
    An earlier patch shows that anything returned by
    gsi_dealloc_channel() other than GSI_STATUS_SUCCESS (or 0) leads
    eventually to common behavior that won't change if the particular
    error code changes.
    
    Change-Id: I6f304ad7c08d1d2bcd8f5ddd955e9f14c98f7046
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/gsi/gsi.c
+++ drivers/platform/msm/ipa_lite/gsi/gsi.c
@@ -1691,7 +1691,7 @@ int gsi_dealloc_channel(unsigned long chan_hdl)
 
 	if (!gsi_ctx) {
 		pr_err("%s:%d gsi context not allocated\n", __func__, __LINE__);
-		return -GSI_STATUS_NODEV;
+		return -ENODEV;
 	}
 
 	if (chan_hdl >= gsi_ctx->max_ch) {
