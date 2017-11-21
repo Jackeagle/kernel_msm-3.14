0937f7c8b797b4f46dc657bd067ff400b1023809
Alex Elder <elder@codeaurora.org>
2017-11-21 09:30:29 -0600

    msm: gsi: use ENODEV in gsi_write_channel_scratch()
    Use ENODEV rather than GSI_STATUS_NODEV in gsi_write_channel_scratch().
    
    gsi_write_channel_scratch() is called in two spots:
      - In ipa3_request_gsi_channel(), if any value is returned other
        than GSI_STATUS_SUCCESS, -EFAULT is returned to its caller.
      - In ipa_gsi_setup_channel() any returned value different from
        GSI_STATUS_SUCCESS is returned to its caller.  A recent patch
        explains why the behavior will be the same with this change if
        ipa_gsi_setup_channel() returns a non-zero value.
    
    So this patch will not change the behavior of the code.
    
    Change-Id: Ie5c51a095a4b51fd6a96b2f0bb5b70ca6e8b5ef9
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/gsi/gsi.c
+++ drivers/platform/msm/ipa_lite/gsi/gsi.c
@@ -1425,7 +1425,7 @@ int gsi_write_channel_scratch(unsigned long chan_hdl,
 
 	if (!gsi_ctx) {
 		pr_err("%s:%d gsi context not allocated\n", __func__, __LINE__);
-		return -GSI_STATUS_NODEV;
+		return -ENODEV;
 	}
 
 	if (chan_hdl >= gsi_ctx->max_ch) {
