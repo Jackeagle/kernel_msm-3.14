08e9c1a98f2bd3a4e856f5dbf746c66f86c2f541
Alex Elder <elder@codeaurora.org>
2017-11-29 17:35:14 -0600

    msm: gsi: more IRQ register refactoring
    Isolate the read/modify/update code in a single function that is
    called by the six functions that operate on interrupt masks.
    
    Change-Id: I33446d1220cba94565ea9a921da02f758446a4d3
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/gsi/gsi.c
+++ drivers/platform/msm/ipa_lite/gsi/gsi.c
@@ -31,64 +31,43 @@
 
 struct gsi_ctx *gsi_ctx;
 
-static void __gsi_config_type_irq(int ee, uint32_t mask, uint32_t val)
+static void gsi_irq_update(uint32_t offset, uint32_t mask, uint32_t val)
 {
-	uint32_t offset = GSI_EE_n_CNTXT_TYPE_IRQ_MSK_OFFS(ee);
 	uint32_t curr;
 
 	curr = gsi_readl(offset);
 	val = (curr & ~mask) | (val & mask);
 	gsi_writel(val, offset);
+}
+
+static void __gsi_config_type_irq(int ee, uint32_t mask, uint32_t val)
+{
+	gsi_irq_update(GSI_EE_n_CNTXT_TYPE_IRQ_MSK_OFFS(ee), mask, val);
 }
 
 static void __gsi_config_ch_irq(int ee, uint32_t mask, uint32_t val)
 {
-	uint32_t offset = GSI_EE_n_CNTXT_SRC_GSI_CH_IRQ_MSK_OFFS(ee);
-	uint32_t curr;
-
-	curr = gsi_readl(offset);
-	val = (curr & ~mask) | (val & mask);
-	gsi_writel(val, offset);
+	gsi_irq_update(GSI_EE_n_CNTXT_SRC_GSI_CH_IRQ_MSK_OFFS(ee), mask, val);
 }
 
 static void __gsi_config_evt_irq(int ee, uint32_t mask, uint32_t val)
 {
-	uint32_t offset = GSI_EE_n_CNTXT_SRC_EV_CH_IRQ_MSK_OFFS(ee);
-	uint32_t curr;
-
-	curr = gsi_readl(offset);
-	val = (curr & ~mask) | (val & mask);
-	gsi_writel(val, offset);
+	gsi_irq_update(GSI_EE_n_CNTXT_SRC_EV_CH_IRQ_MSK_OFFS(ee), mask, val);
 }
 
 static void __gsi_config_ieob_irq(int ee, uint32_t mask, uint32_t val)
 {
-	uint32_t offset = GSI_EE_n_CNTXT_SRC_IEOB_IRQ_MSK_OFFS(ee);
-	uint32_t curr;
-
-	curr = gsi_readl(offset);
-	val = (curr & ~mask) | (val & mask);
-	gsi_writel(val, offset);
+	gsi_irq_update(GSI_EE_n_CNTXT_SRC_IEOB_IRQ_MSK_OFFS(ee), mask, val);
 }
 
 static void __gsi_config_glob_irq(int ee, uint32_t mask, uint32_t val)
 {
-	uint32_t offset = GSI_EE_n_CNTXT_GLOB_IRQ_EN_OFFS(ee);
-	uint32_t curr;
-
-	curr = gsi_readl(offset);
-	val = (curr & ~mask) | (val & mask);
-	gsi_writel(val, offset);
+	gsi_irq_update(GSI_EE_n_CNTXT_GLOB_IRQ_EN_OFFS(ee), mask, val);
 }
 
 static void __gsi_config_gen_irq(int ee, uint32_t mask, uint32_t val)
 {
-	uint32_t offset = GSI_EE_n_CNTXT_GSI_IRQ_EN_OFFS(ee);
-	uint32_t curr;
-
-	curr = gsi_readl(offset);
-	val = (curr & ~mask) | (val & mask);
-	gsi_writel(val, offset);
+	gsi_irq_update(GSI_EE_n_CNTXT_GSI_IRQ_EN_OFFS(ee), mask, val);
 }
 
 static void gsi_handle_ch_ctrl(int ee)
