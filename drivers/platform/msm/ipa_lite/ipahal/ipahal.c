792c0447b8c0e03bd69ea7245de897cc8005286d
Alex Elder <elder@codeaurora.org>
2017-12-30 09:25:21 -0600

    msm: ipahal: don't put zero immcmd_obj on the stack
    In ipahal_imm_cmd_init() there's an object defined whose purpose is
    just to be zero-filled, for comparison against other objects.  Give
    that object static lifetime rather than putting it on the stack.
    This avoids consuming the stack space and has the side-effect of
    being initially zero-filled.  Make it const as well.
    
    Change-Id: I492de3717886726223f087a9b01e17b6d987481e
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipahal/ipahal.c
+++ drivers/platform/msm/ipa_lite/ipahal/ipahal.c
@@ -686,11 +686,10 @@ static int ipahal_imm_cmd_init(enum ipa_hw_type ipa_hw_type)
 {
 	int i;
 	int j;
-	struct ipahal_imm_cmd_obj zero_obj;
+	static const struct ipahal_imm_cmd_obj zero_obj;
 
 	ipa_debug_low("Entry - HW_TYPE=%d\n", ipa_hw_type);
 
-	memset(&zero_obj, 0, sizeof(zero_obj));
 	for (i = IPA_HW_v3_0 ; i < ipa_hw_type ; i++) {
 		for (j = 0; j < IPA_IMM_CMD_MAX ; j++) {
 			if (!memcmp(&ipahal_imm_cmd_objs[i+1][j], &zero_obj,
