de96fe30b09a8dd1c7ed39636787c3255332a0d0
Alex Elder <elder@codeaurora.org>
2018-01-03 16:23:41 -0600

    msm: ipahal: check params in ipa_imm_cmd_construct_ip_v4_nat_init()
    Three fields in an IP_V4_NAT_INIT command are limited to a size
    that is smaller than the parameters provided to fill them in
    ipa_imm_cmd_construct_ip_v4_nat_init().  Add checks to catch
    attempts to use values that are too big.
    
    Change-Id: I7c39300c05bf1c023f480ff82709349074218eb2
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipahal/ipahal.c
+++ drivers/platform/msm/ipa_lite/ipahal/ipahal.c
@@ -448,6 +448,13 @@ ipa_imm_cmd_construct_ip_v4_nat_init(u16 opcode, const void *params)
 	struct ipa_imm_cmd_hw_ip_v4_nat_init *data;
 	const struct ipahal_imm_cmd_ip_v4_nat_init *nat4_params = params;
 
+	if (check_too_big("Tble index", nat4_params->table_index, 3))
+		return NULL;
+	if (check_too_big("Sz basetbls", nat4_params->size_base_tables, 12))
+		return NULL;
+	if (check_too_big("Sz exptbls", nat4_params->size_expansion_tables, 10))
+		return NULL;
+
 	pyld = ipahal_imm_cmd_pyld_alloc(opcode, sizeof(*data));
 	if (!pyld)
 		return NULL;
