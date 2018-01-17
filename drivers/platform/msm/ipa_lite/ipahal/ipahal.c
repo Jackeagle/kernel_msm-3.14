eed8e134956d9b10b2b67de532c69233844ec0ad
Alex Elder <elder@codeaurora.org>
2018-01-17 14:13:32 -0600

    msm: ipahal: ipa_hw_type is IPA_HW_v3_5_1 in ipahal_init()
    There's no need to verify the hardware version passed to
    ipahal_init().  We can see by inspection it will be IPA_HW_v3_5_1.
    
    Change-Id: I65deaf0622947d55488aaf8ac0898ccbe76a1053
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipahal/ipahal.c
+++ drivers/platform/msm/ipa_lite/ipahal/ipahal.c
@@ -939,11 +939,6 @@ int ipahal_init(enum ipa_hw_type ipa_hw_type, void __iomem *base)
 {
 	ipa_debug("Entry - IPA HW TYPE=%d base=%p\n", ipa_hw_type, base);
 
-	if (ipa_hw_type != IPA_HW_v3_5_1) {
-		ipa_err("ipahal supported on IPAv3.5.1 only\n");
-		return -EINVAL;
-	}
-
 	ipahal_ctx->hw_type = ipa_hw_type;
 	ipahal_ctx->base = base;
 	/* ipahal_ctx->ipa_pdev must be set by a call to ipahal_dev_init() */
