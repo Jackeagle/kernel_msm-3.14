acb9d22104239537e6f74aaa9a182e2f162b7923
Alex Elder <elder@codeaurora.org>
2018-01-19 13:03:58 -0600

    msm: ipa: clean up ipa3_get_ep_mapping()
    Use ipa3_get_gsi_ep_info() rather than ep_configuration() to access
    a client's endpoint info structure.  Rearrange the logic a bit, and
    make some minor formatting changes to improve readability.
    
    Change-Id: Ife0a90b950d9047b5ac792b7c1bf63326299aec1
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_utils.c
+++ drivers/platform/msm/ipa_lite/ipa_utils.c
@@ -723,18 +723,20 @@ ipa3_get_gsi_ep_info(enum ipa_client_type client)
  */
 int ipa3_get_ep_mapping(enum ipa_client_type client)
 {
-	const struct ipa_ep_configuration *ep_config;
+	const struct ipa_gsi_ep_config *ep_info;
 	int ipa_ep_idx;
 
-	ep_config = ep_configuration(client);
-	if (!ep_config)
+	ep_info = ipa3_get_gsi_ep_info(client);
+	if (!ep_info)
 		return -ESRCH;
 
-	ipa_ep_idx = ep_config->ipa_gsi_ep_info.ipa_ep_num;
-	if (ipa_ep_idx < 0 || (ipa_ep_idx >= IPA3_MAX_NUM_PIPES
-		&& client != IPA_CLIENT_DUMMY_CONS))
+	ipa_ep_idx = ep_info->ipa_ep_num;
+	if (ipa_ep_idx < 0)
 		return -ENOENT;
-	return ipa_ep_idx;
+	if (ipa_ep_idx < IPA3_MAX_NUM_PIPES || client == IPA_CLIENT_DUMMY_CONS)
+		return ipa_ep_idx;
+
+	return -ENOENT;
 }
 
 /**
