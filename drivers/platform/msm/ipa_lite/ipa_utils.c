3ed1f1dc03f4ce177ecfe557eb51513b5c74dcf7
Alex Elder <elder@codeaurora.org>
2018-01-19 14:08:14 -0600

    msm: ipa: use client_handle_valid() in ipa3_cfg_ep_hdr()
    Simplify the check at the top of ipa3_cfg_ep_hdr() by using
    client_handle_valid().
    
    Change-Id: I16f6b428282cca1dfb1a5d9fe959e1b60876f3ea
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_utils.c
+++ drivers/platform/msm/ipa_lite/ipa_utils.c
@@ -1080,12 +1080,9 @@ int ipa3_cfg_ep_hdr(u32 clnt_hdl, const struct ipa_ep_cfg_hdr *ep_hdr)
 {
 	struct ipa3_ep_context *ep;
 
-	if (clnt_hdl >= ipa3_ctx->ipa_num_pipes ||
-	    ipa3_ctx->ep[clnt_hdl].valid == 0) {
-		ipa_err("bad parm, clnt_hdl = %d , ep_valid = %d\n",
-				clnt_hdl, ipa3_ctx->ep[clnt_hdl].valid);
+	if (!client_handle_valid(clnt_hdl))
 		return -EINVAL;
-	}
+
 	ipa_debug("pipe=%d metadata_reg_valid=%d\n",
 		clnt_hdl,
 		ep_hdr->hdr_metadata_reg_valid);
