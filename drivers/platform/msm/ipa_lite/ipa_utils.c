19e4eebe81b40cdaff842035116b69f8af5ecbdc
Alex Elder <elder@codeaurora.org>
2018-01-19 14:06:56 -0600

    msm: ipa: ep_hdr is non-null in ipa3_cfg_ep_hdr()
    The only caller of ipa3_cfg_ep_hdr() passes the address of an object
    as the ep_hdr parameter.  This will never be null, so there's no
    need to check for that condition in the function.
    
    Change-Id: Ib7002569622c94e33bc39f67805f66acdd55a7cf
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_utils.c
+++ drivers/platform/msm/ipa_lite/ipa_utils.c
@@ -1081,7 +1081,7 @@ int ipa3_cfg_ep_hdr(u32 clnt_hdl, const struct ipa_ep_cfg_hdr *ep_hdr)
 	struct ipa3_ep_context *ep;
 
 	if (clnt_hdl >= ipa3_ctx->ipa_num_pipes ||
-	    ipa3_ctx->ep[clnt_hdl].valid == 0 || ep_hdr == NULL) {
+	    ipa3_ctx->ep[clnt_hdl].valid == 0) {
 		ipa_err("bad parm, clnt_hdl = %d , ep_valid = %d\n",
 				clnt_hdl, ipa3_ctx->ep[clnt_hdl].valid);
 		return -EINVAL;
