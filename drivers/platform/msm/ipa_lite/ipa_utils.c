afe39bfcd5471f7b622165436c3c9ced9743ab8d
Alex Elder <elder@codeaurora.org>
2018-01-19 13:52:11 -0600

    msm: ipa: metadata_mask is non-null in ipa3_cfg_ep_metadata_mask()
    The only caller of ipa3_cfg_ep_metadata_mask() passes the address of
    an object as the metadata_mask parameter.  This will never be null,
    so there's no need to check for that condition in the function.
    
    Change-Id: If8fa8eb36bd705303d77530bbcbc90fefc31d34a
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_utils.c
+++ drivers/platform/msm/ipa_lite/ipa_utils.c
@@ -918,7 +918,7 @@ static int ipa3_cfg_ep_metadata_mask(u32 clnt_hdl,
 		*metadata_mask)
 {
 	if (clnt_hdl >= ipa3_ctx->ipa_num_pipes ||
-	    ipa3_ctx->ep[clnt_hdl].valid == 0 || metadata_mask == NULL) {
+	    ipa3_ctx->ep[clnt_hdl].valid == 0) {
 		ipa_err("bad parm, clnt_hdl = %d , ep_valid = %d\n",
 					clnt_hdl,
 					ipa3_ctx->ep[clnt_hdl].valid);
