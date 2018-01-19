9120235afabd7fcac3e0455b401fa79a8bad07e7
Alex Elder <elder@codeaurora.org>
2018-01-19 16:17:34 -0600

    msm: ipa: tuple is non-null in ipa3_set_flt_tuple_mask()
    There is only one caller of ipa3_set_flt_tuple_mask(), and it
    supplies the address of an object as its "tuple" argument.  That
    will never be null, so there's no need to test for that.
    
    Change-Id: I69256d3e11b74cea574ba00e6bafba92ce17c1b1
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_utils.c
+++ drivers/platform/msm/ipa_lite/ipa_utils.c
@@ -2836,11 +2836,6 @@ int ipa3_set_flt_tuple_mask(int pipe_idx, struct ipahal_reg_hash_tuple *tuple)
 {
         struct ipahal_reg_fltrt_hash_tuple fltrt_tuple;
 
-        if (!tuple) {
-                ipa_err("bad tuple\n");
-                return -EINVAL;
-        }
-
         if (pipe_idx >= ipa3_ctx->ipa_num_pipes || pipe_idx < 0) {
                 ipa_err("bad pipe index!\n");
                 return -EINVAL;
