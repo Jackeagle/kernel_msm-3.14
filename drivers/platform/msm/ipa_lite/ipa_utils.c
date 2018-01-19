f62d25cc4d60b2c19d6324ec1ed75862091b921e
Alex Elder <elder@codeaurora.org>
2018-01-19 14:04:53 -0600

    msm: ipa: cfg is non-null in ipa3_cfg_ep_cfg()
    The only caller of ipa3_cfg_ep_cfg() passes the address of an object
    as the cfg parameter.  This will never be null, so there's no need
    to check for that condition in the function.
    
    Change-Id: I930b4669eb905822f7c603b67563e0809c3223df
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_utils.c
+++ drivers/platform/msm/ipa_lite/ipa_utils.c
@@ -1041,7 +1041,7 @@ int ipa3_cfg_ep_cfg(u32 clnt_hdl, const struct ipa_ep_cfg_cfg *cfg)
 	u8 qmb_master_sel;
 
 	if (clnt_hdl >= ipa3_ctx->ipa_num_pipes ||
-	    ipa3_ctx->ep[clnt_hdl].valid == 0 || cfg == NULL) {
+	    ipa3_ctx->ep[clnt_hdl].valid == 0) {
 		ipa_err("bad parm, clnt_hdl = %d , ep_valid = %d\n",
 					clnt_hdl,
 					ipa3_ctx->ep[clnt_hdl].valid);
