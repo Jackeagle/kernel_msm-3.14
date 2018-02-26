460ff80ec5122fd4ddc41b24954a4bb2ef804316
Alex Elder <elder@codeaurora.org>
2018-02-26 08:09:54 -0600

    msm: ipa: update log in _ipa_client_remove_wait()
    There are currently no callers, but have _ipa_client_remove_wait()
    call ipa3_active_clients_log_mod() to match usage of the other four
    client reference functions.
    
    Change-Id: I3a74170e66284a38531a73908c5fd2f6b6dcdef6
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa.c
+++ drivers/platform/msm/ipa_lite/ipa.c
@@ -1304,6 +1304,13 @@ void _ipa_client_remove(const char *id, bool log_it, const char *file, int line)
 void
 _ipa_client_remove_wait(const char *id, bool log_it, const char *file, int line)
 {
+	struct ipa_active_client_logging_info log_info;
+
+	log_info.id_string = id;
+	log_info.file = file;
+	log_info.line = line;
+	ipa3_active_clients_log_mod(&log_info, log_it, false);
+
 	if (!ipa_client_remove_not_final())
 		ipa_client_remove_final();
 }
