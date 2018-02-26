fa1adc77725f49d8c9ceb3b4f42998935ed99f7c
Alex Elder <elder@codeaurora.org>
2018-02-26 09:45:58 -0600

    msm: ipa: kill IPA_ACTIVE_CLIENTS_{INC,DEC}_RESOURCE()
    IPA_ACTIVE_CLIENTS_INC_RESOURCE() and IPA_ACTIVE_CLIENTS_DEC_RESOURCE()
    are trivial wrapper macros.  There are no callers, so we can just
    delete these macros with no other change.
    
    Change-Id: I30c27494ef220b6dc6a841eb6d5297b8aa635348
    Signed-off-by: Alex Elder <elder@codeaurora.org>

--- drivers/platform/msm/ipa_lite/ipa_common_i.h
+++ drivers/platform/msm/ipa_lite/ipa_common_i.h
@@ -54,16 +54,6 @@ static __always_inline u32 field_val(u32 reg, u32 field_mask)
 	return (reg & field_mask) >> field_shift(field_mask);
 }
 
-#define IPA_ACTIVE_CLIENTS_INC_RESOURCE(resource_name) \
-	do { \
-		ipa_client_add(resource_name, true); \
-	} while (0)
-
-#define IPA_ACTIVE_CLIENTS_DEC_RESOURCE(resource_name) \
-	do { \
-		ipa_client_remove(resource_name, true); \
-	} while (0)
-
 #define IPA_ACTIVE_CLIENTS_INC_SPECIAL(id_str) \
 	do { \
 		ipa_client_add(id_str, true); \
