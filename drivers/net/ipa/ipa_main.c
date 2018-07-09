15b1e044a9b687a4fa5e55f930c14c444f8e2e84
Alex Elder <elder@linaro.org>
2018-07-09 07:50:06 -0500

    soc: qcom: ipa: kill V4_FLT_NHASH_{OFST,SIZE}
    Remove the definitions of V4_FLT_NHASH_OFST and V4_FLT_NHASH_SIZE,
    which are used indexes into the ipa_mem[] array.  Use the constants
    IPA_MEM_V4_FLT_NHASH_OFST and IPA_MEM_V4_FLT_NHASH_SIZE wherever those
    array entries are used instead.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipa_main.c
+++ drivers/net/ipa/ipa_main.c
@@ -534,7 +534,7 @@ static int ipa_init_flt4(void)
 
 	hash_offset = ipa_ctx->smem_restricted_bytes + IPA_MEM_V4_FLT_HASH_OFST;
 	nhash_offset = ipa_ctx->smem_restricted_bytes +
-					ipa_ctx->mem_info[V4_FLT_NHASH_OFST];
+					IPA_MEM_V4_FLT_NHASH_OFST;
 	cmd_pyld = ipahal_ip_v4_filter_init_pyld(&mem, hash_offset,
 						 nhash_offset);
 	if (!cmd_pyld) {
@@ -1561,9 +1561,9 @@ static bool config_valid(void)
 			IPA_MEM_V4_FLT_HASH_SIZE, table_count, width);
 		return false;
 	}
-	if (ipa_ctx->mem_info[V4_FLT_NHASH_SIZE] < required_size) {
+	if (IPA_MEM_V4_FLT_NHASH_SIZE < required_size) {
 		ipa_err("V4_FLT_NHASH_SIZE too small (%u < %u * %u)\n",
-			ipa_ctx->mem_info[V4_FLT_NHASH_SIZE], table_count,
+			IPA_MEM_V4_FLT_NHASH_SIZE, table_count,
 			width);
 		return false;
 	}
@@ -1897,8 +1897,6 @@ static void ipa_init_mem_info(u32 *mem_info)
 	BUILD_BUG_ON(IPA_MEM_MODEM_OFST % 8);
 	BUILD_BUG_ON(IPA_MEM_UC_EVENT_RING_OFST % 1024);
 
-	mem_info[V4_FLT_NHASH_OFST] = IPA_MEM_V4_FLT_NHASH_OFST;
-	mem_info[V4_FLT_NHASH_SIZE] = IPA_MEM_V4_FLT_NHASH_SIZE;
 	mem_info[V6_FLT_HASH_OFST] = IPA_MEM_V6_FLT_HASH_OFST;
 	mem_info[V6_FLT_HASH_SIZE] = IPA_MEM_V6_FLT_HASH_SIZE;
 	mem_info[V6_FLT_NHASH_OFST] = IPA_MEM_V6_FLT_NHASH_OFST;
--- drivers/net/ipa/ipa_qmi.c
+++ drivers/net/ipa/ipa_qmi.c
@@ -265,7 +265,7 @@ static const struct ipa_init_modem_driver_req *init_modem_driver_req(void)
 	req.v6_route_tbl_info.count = ipa_ctx->mem_info[V6_MODEM_RT_INDEX_HI];
 
 	req.v4_filter_tbl_start_valid = true;
-	req.v4_filter_tbl_start = base + ipa_ctx->mem_info[V4_FLT_NHASH_OFST];
+	req.v4_filter_tbl_start = base + IPA_MEM_V4_FLT_NHASH_OFST;
 
 	req.v6_filter_tbl_start_valid = true;
 	req.v6_filter_tbl_start = base + ipa_ctx->mem_info[V6_FLT_NHASH_OFST];
