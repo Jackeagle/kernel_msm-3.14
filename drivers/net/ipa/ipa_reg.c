62d26bf98fb103cd24b9226cd8eb5aa399d6c0fd
Alex Elder <elder@linaro.org>
2018-09-09 08:03:31 -0500

    soc: qcom: ipa: fix pr_fmt() in "ipa_reg.c"
    Fix the definition of pr_fmt() in "ipa_reg.c" to use "ipa_reg"
    rather than "ipahal".
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipa_reg.c
+++ drivers/net/ipa/ipa_reg.c
@@ -3,7 +3,7 @@
 /* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2018 Linaro Ltd.
  */
-#define pr_fmt(fmt)	"ipahal %s:%d " fmt, __func__, __LINE__
+#define pr_fmt(fmt)	"ipa_reg %s:%d " fmt, __func__, __LINE__
 
 #include <linux/io.h>
 
