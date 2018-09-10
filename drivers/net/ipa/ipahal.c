4056c6cf8a47fe6f65ee396b416a9dc04dc87845
Alex Elder <elder@linaro.org>
2018-09-09 19:51:51 -0500

    soc: qcom: ipa: dest_pipe_idx is in range in ipahal_ip_packet_init_pyld()
    The dest_pipe_idx value passed to ipahal_ip_packet_init_pyld() has
    to fit in a 5 bit field.  A check_too_big() call in that function
    checks this, but we can simply replace that check with an assertion.
    
    There are currently no callers of ipahal_ip_packet_init_pyld(), so
    there's no need to verify this assertion will hold.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipahal.c
+++ drivers/net/ipa/ipahal.c
@@ -153,8 +153,8 @@ struct ipahal_imm_cmd_pyld *ipahal_ip_packet_init_pyld(u32 dest_pipe_idx)
 	struct ipa_imm_cmd_hw_ip_packet_init *data;
 	u16 opcode;
 
-	if (check_too_big("dest_pipe_idx", dest_pipe_idx, 5))
-		return NULL;
+	/* destination_pipe_index is 5 bits wide */
+	ipa_assert(dest_pipe_idx < 1 << 5);
 
 	opcode = IPA_IMM_CMD_IP_PACKET_INIT;
 	pyld = ipahal_imm_cmd_pyld_alloc(opcode, sizeof(*data));
