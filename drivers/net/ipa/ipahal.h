1461cb786e721f4a51f7c3cc38811521ed110c4e
Alex Elder <elder@linaro.org>
2018-10-30 09:38:33 -0500

    soc: qcom: ipa: kill "ipa_common_i.h"
    Move the content that remains in "ipa_common_i.h" into "ipa_i.h".
    Stop including "ipa_common_i.h", and remove it.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipahal.h
+++ drivers/net/ipa/ipahal.h
@@ -5,8 +5,8 @@
  */
 #ifndef _IPAHAL_H_
 #define _IPAHAL_H_
+
 #include <linux/if_ether.h>
-#include "ipa_common_i.h"
 
 /* The IPA implements offloaded packet filtering and routing
  * capabilities.  This is managed by programming IPA-resident
