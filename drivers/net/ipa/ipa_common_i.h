45dce2531d37f17dd714d4f7e084343040369e26
Alex Elder <elder@linaro.org>
2018-10-30 09:20:14 -0500

    soc: qcom: ipa: remove unused definitions in "ipa_common_i.h"
    Delete a bunch of the ipa_client_type enumerated values that are
    never used in the code.  Remove the ipa_active_client_logging_info
    structure and hdr_total_len_or_pad_type enumerated type, which are
    no longer required.  And get rid of the two headers--they're not
    needed either.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipa_common_i.h
+++ drivers/net/ipa/ipa_common_i.h
@@ -6,10 +6,6 @@
 #ifndef _IPA_COMMON_I_H_
 #define _IPA_COMMON_I_H_
 
-#include <linux/slab.h>
-
-#include "ipa_reg.h"
-
 /** enum ipa_irq_type - IPA Interrupt Type
  * Used to register handlers for IPA interrupts
  *
@@ -31,81 +27,25 @@ enum ipa_irq_type {
  * Add new clients in the end of the list and update IPA_CLIENT_MAX
  */
 enum ipa_client_type {
-	IPA_CLIENT_HSIC1_PROD                   = 0,
-	IPA_CLIENT_HSIC1_CONS                   = 1,
-
-	IPA_CLIENT_HSIC2_PROD                   = 2,
-	IPA_CLIENT_HSIC2_CONS                   = 3,
-
-	IPA_CLIENT_HSIC3_PROD                   = 4,
-	IPA_CLIENT_HSIC3_CONS                   = 5,
-
-	IPA_CLIENT_HSIC4_PROD                   = 6,
-	IPA_CLIENT_HSIC4_CONS                   = 7,
-
-	IPA_CLIENT_HSIC5_PROD                   = 8,
-	IPA_CLIENT_HSIC5_CONS                   = 9,
-
 	IPA_CLIENT_WLAN1_PROD                   = 10,
 	IPA_CLIENT_WLAN1_CONS                   = 11,
 
-	IPA_CLIENT_A5_WLAN_AMPDU_PROD           = 12,
 	IPA_CLIENT_WLAN2_CONS                   = 13,
 
-	/* RESERVERD PROD                       = 14, */
 	IPA_CLIENT_WLAN3_CONS                   = 15,
-
-	/* RESERVERD PROD                       = 16, */
-	IPA_CLIENT_WLAN4_CONS                   = 17,
 
 	IPA_CLIENT_USB_PROD                     = 18,
 	IPA_CLIENT_USB_CONS                     = 19,
 
-	IPA_CLIENT_USB2_PROD                    = 20,
-	IPA_CLIENT_USB2_CONS                    = 21,
-
-	IPA_CLIENT_USB3_PROD                    = 22,
-	IPA_CLIENT_USB3_CONS                    = 23,
-
-	IPA_CLIENT_USB4_PROD                    = 24,
-	IPA_CLIENT_USB4_CONS                    = 25,
-
-	IPA_CLIENT_UC_USB_PROD                  = 26,
 	IPA_CLIENT_USB_DPL_CONS                 = 27,
-
-	IPA_CLIENT_A2_EMBEDDED_PROD		= 28,
-	IPA_CLIENT_A2_EMBEDDED_CONS		= 29,
-
-	IPA_CLIENT_A2_TETHERED_PROD             = 30,
-	IPA_CLIENT_A2_TETHERED_CONS             = 31,
 
 	IPA_CLIENT_APPS_LAN_PROD		= 32,
 	IPA_CLIENT_APPS_LAN_CONS		= 33,
 
 	IPA_CLIENT_APPS_WAN_PROD		= 34,
-	IPA_CLIENT_APPS_LAN_WAN_PROD = IPA_CLIENT_APPS_WAN_PROD,
 	IPA_CLIENT_APPS_WAN_CONS		= 35,
 
 	IPA_CLIENT_APPS_CMD_PROD		= 36,
-	IPA_CLIENT_A5_LAN_WAN_CONS		= 37,
-
-	IPA_CLIENT_ODU_PROD                     = 38,
-	IPA_CLIENT_ODU_EMB_CONS                 = 39,
-
-	/* RESERVERD PROD                       = 40, */
-	IPA_CLIENT_ODU_TETH_CONS                = 41,
-
-	IPA_CLIENT_MHI_PROD                     = 42,
-	IPA_CLIENT_MHI_CONS                     = 43,
-
-	IPA_CLIENT_MEMCPY_DMA_SYNC_PROD		= 44,
-	IPA_CLIENT_MEMCPY_DMA_SYNC_CONS		= 45,
-
-	IPA_CLIENT_MEMCPY_DMA_ASYNC_PROD	= 46,
-	IPA_CLIENT_MEMCPY_DMA_ASYNC_CONS	= 47,
-
-	IPA_CLIENT_ETHERNET_PROD                = 48,
-	IPA_CLIENT_ETHERNET_CONS                = 49,
 
 	IPA_CLIENT_Q6_LAN_PROD			= 50,
 	IPA_CLIENT_Q6_LAN_CONS			= 51,
@@ -114,16 +54,6 @@ enum ipa_client_type {
 	IPA_CLIENT_Q6_WAN_CONS			= 53,
 
 	IPA_CLIENT_Q6_CMD_PROD			= 54,
-	IPA_CLIENT_Q6_DUN_CONS			= 55,
-
-	IPA_CLIENT_Q6_DECOMP_PROD		= 56,
-	IPA_CLIENT_Q6_DECOMP_CONS		= 57,
-
-	IPA_CLIENT_Q6_DECOMP2_PROD		= 58,
-	IPA_CLIENT_Q6_DECOMP2_CONS		= 59,
-
-	/* RESERVERD PROD			= 60, */
-	IPA_CLIENT_Q6_LTE_WIFI_AGGR_CONS	= 61,
 
 	IPA_CLIENT_TEST_PROD                    = 62,
 	IPA_CLIENT_TEST_CONS                    = 63,
@@ -140,7 +70,6 @@ enum ipa_client_type {
 	IPA_CLIENT_TEST4_PROD                   = 70,
 	IPA_CLIENT_TEST4_CONS                   = 71,
 
-	/* RESERVERD PROD			= 72, */
 	IPA_CLIENT_DUMMY_CONS			= 73,
 
 	IPA_CLIENT_MAX,
@@ -178,12 +107,6 @@ static inline bool ipa_ap_consumer(enum ipa_client_type client)
 		client == IPA_CLIENT_APPS_WAN_CONS;
 }
 
-struct ipa_active_client_logging_info {
-	const char *id_string;
-	const char *file;
-	int line;
-};
-
 /** enum ipa_dp_evt_type - type of event client callback is
  * invoked for on data path
  * @IPA_RECEIVE: data is struct sk_buff
@@ -199,16 +122,6 @@ enum ipa_dp_evt_type {
 #define IPA_GENERIC_AGGR_BYTE_LIMIT	(6 * SZ_1K)	/* bytes */
 #define IPA_GENERIC_AGGR_TIME_LIMIT	1		/* milliseconds */
 #define IPA_GENERIC_AGGR_PKT_LIMIT	0
-
-/** enum hdr_total_len_or_pad_type - type of value held by TOTAL_LEN_OR_PAD
- * field in header configuration register.
- * @IPA_HDR_PAD: field is used as padding length
- * @IPA_HDR_TOTAL_LEN: field is used as total length
- */
-enum hdr_total_len_or_pad_type {
-	IPA_HDR_PAD		= 0,
-	IPA_HDR_TOTAL_LEN	= 1,
-};
 
 #define ipa_debug(fmt, args...)	dev_dbg(ipa_ctx->dev, fmt, ## args)
 #define ipa_err(fmt, args...)	dev_err(ipa_ctx->dev, fmt, ## args)
