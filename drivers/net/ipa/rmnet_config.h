2420f69b6f0fd5a1352cb1c0e5207d91ca8e37b0
Alex Elder <elder@linaro.org>
2018-11-02 15:14:32 -0500

    soc: qcom: ipa: remove unused definitions in "rmnet_config.h"
    Remove definitions in "rmnet_config.h" that are never used.
    
    Add a comment indicating we want to use definitions that already
    exist elsewhere in the tree.  (The header file with them needs to be
    moved into /usr/include.)
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/rmnet_config.h
+++ drivers/net/ipa/rmnet_config.h
@@ -6,13 +6,12 @@
 #ifndef _RMNET_CONFIG_H_
 #define _RMNET_CONFIG_H_
 
-#include <linux/skbuff.h>
+#include <linux/types.h>
 
-struct rmnet_phys_ep_conf_s {
-	void	(*recycle)(struct sk_buff *); /* Destruct function */
-	void	*config;
-};
-
+/* XXX We want to use struct rmnet_map_header, but that's currently defined in
+ * XXX     drivers/net/ethernet/qualcomm/rmnet/rmnet_map.h
+ * XXX We also want to use RMNET_MAP_GET_CD_BIT(Y), defined in the same file.
+ */
 struct rmnet_map_header_s {
 #ifndef RMNET_USE_BIG_ENDIAN_STRUCTS
 	u8	pad_len		: 6,
@@ -27,12 +26,6 @@ struct rmnet_map_header_s {
 	u16	pkt_len;
 }  __aligned(1);
 
-#define RMNET_MAP_GET_MUX_ID(Y) (((struct rmnet_map_header_s *)Y->data)->mux_id)
 #define RMNET_MAP_GET_CD_BIT(Y) (((struct rmnet_map_header_s *)Y->data)->cd_bit)
-#define RMNET_MAP_GET_PAD(Y) (((struct rmnet_map_header_s *)Y->data)->pad_len)
-#define RMNET_MAP_GET_CMD_START(Y) ((struct rmnet_map_control_command_s *) \
-				  (Y->data + sizeof(struct rmnet_map_header_s)))
-#define RMNET_MAP_GET_LENGTH(Y) (ntohs( \
-			       ((struct rmnet_map_header_s *)Y->data)->pkt_len))
 
 #endif /* _RMNET_CONFIG_H_ */
