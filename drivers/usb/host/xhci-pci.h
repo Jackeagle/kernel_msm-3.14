6dcd0900fded602ce4e84066efdeb70c2c8ada6c
Christian Lamparter <chunkeey@googlemail.com>
2020-03-23 13:48:45 +0530

    usb: renesas-xhci: Add the renesas xhci driver
    This add a new driver for renesas xhci which is basically a firmware
    loader for uPD720201 and uPD720202 w/o ROM. It uses xhci-pci driver for
    most of the work.
    
    This is added in Makefile before the xhci-pci driver so that it first
    get probed for renesas devices before the xhci-pci driver has a chance
    to claim any device.
    
    This patch adds a firmware loader for the uPD720201K8-711-BAC-A
    and uPD720202K8-711-BAA-A variant. Both of these chips are listed
    in Renesas' R19UH0078EJ0500 Rev.5.00 "User's Manual: Hardware" as
    devices which need the firmware loader on page 2 in order to
    work as they "do not support the External ROM".
    
    The "Firmware Download Sequence" is describe in chapter
    "7.1 FW Download Interface" R19UH0078EJ0500 Rev.5.00 page 131.
    
    The firmware "K2013080.mem" is available from a USB3.0 Host to
    PCIe Adapter (PP2U-E card) "Firmware download" archive. An
    alternative version can be sourced from Netgear's WNDR4700 GPL
    archives.
    
    The release notes of the PP2U-E's "Firmware Download" ver 2.0.1.3
    (2012-06-15) state that the firmware is for the following devices:
     - uPD720201 ES 2.0 sample whose revision ID is 2.
     - uPD720201 ES 2.1 sample & CS sample & Mass product, ID is 3.
     - uPD720202 ES 2.0 sample & CS sample & Mass product, ID is 2.
    
    Cc: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    [vkoul: fixed comments:
    	used macros for timeout count and delay
    	removed renesas_fw_alive_check
    	cleaned renesas_fw_callback
    	removed recursion for renesas_fw_download
    	add register defines and field names
    	move to a separate file]
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

---
+++ drivers/usb/host/xhci-pci.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2019-2020 Linaro Limited */
+
+#ifndef XHCI_PCI_H
+#define XHCI_PCI_H
+
+int renesas_xhci_pci_probe(struct pci_dev *dev,
+			   const struct pci_device_id *id);
+int renesas_xhci_pci_remove(struct pci_dev *dev);
+#endif
+
